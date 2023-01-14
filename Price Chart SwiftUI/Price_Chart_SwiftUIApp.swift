db10896a67941f47a0869cc058f14fddb691182f
Chithian Samony <chithiansamony@gmail.com>
2023-01-14 09:16:34 +0700

    Initial Commit

---
+++ Price Chart SwiftUI/Price_Chart_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Price_Chart_SwiftUIApp.swift
+//  Price Chart SwiftUI
+//
+//  Created by Samony Chithian on 14/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Price_Chart_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
